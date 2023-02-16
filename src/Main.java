f5fe19ba795c8a44c037493aca1ff1b5a5c15c7e
Master Howl <125402911+MasterHowl5944@users.noreply.github.com>
2023-02-16 15:06:53 -0600

    Beginning chapter 5

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,31 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        double studentGPA = 3.45;
-        String studentFirstName = "Kayla";
-        String studentLastName = "Miller";
-        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
-        System.out.println("What do you want to update it to?");
-        Scanner input = new Scanner(System.in);
-        studentGPA = input.nextDouble();
+        String question = "Who is Shepard's best friend in the galaxy?";
+        String choiceOne = "Wrex";
+        String choiceTwo = "Tali";
+        String choiceThree = "Garrus";
 
-        System.out.println(studentFirstName + " " + studentLastName + " now has a GPA of " + studentGPA);
+        String correctAnswer = choiceThree;
+
+        //Write a print statement asking the question
+        System.out.println(question);
+        //Write a print statement giving the answer choices
+        System.out.println("Choose one of the following: " + choiceOne + ", " + choiceTwo + ", " + choiceThree+ ".");
+        //Have the user input an answer
+        Scanner input = new Scanner(System.in);
+        //Retrieve the user's input
+        String answer = input.nextLine();
+
+        //If the user's input matches the correct answer
+        //then the user is correct and we want to print out a congrats message to the user
+        //If the user's input does not match the correct answer
+        //then the user is incorrect and we want to print out an error message to the user
+
+        if (correctAnswer.equalsIgnoreCase(answer)) {
+            System.out.println("Correct!");
+        } else {
+            System.out.println("Incorrect!");
+        }
     }
 }
